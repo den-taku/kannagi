84d5e2ad82c33ab691b325b697ab98218f078521
den-taku <den-taku@users.noreply.github.com>
2022-06-30 21:56:30 +0000

    deploy: d011c24332f4438c69c5851dc3cb0ce7998411bd

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"rust_template":{"doc":"","t":[5],"n":["main"],"q":["rust_template"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
