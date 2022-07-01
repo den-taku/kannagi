0af903e99231be620f7e2776989ab144c77138fd
den-taku <dentaku1412@gmail.com>
2022-07-01 18:28:23 +0900

    #1 Add devices crate

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ async fn main() -> std::io::Result<()> {
     kannagi::utils::logging::configure_logger();
     debug!("logger configured.");
 
+    kannagi::devices::configure_devices()?;
+
     info!("Server running!");
     let port = 8080;
     kannagi::server::run_server(port).await
