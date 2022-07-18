31fe71d277cf1ffa67581444d56af491a5d42a1e
den-taku <dentaku1412@gmail.com>
2022-07-19 04:57:26 +0900

    #3 Use rodio instead of ears

--- src/devices/audio.rs
+++ src/devices/audio.rs
@@ -1,9 +1,14 @@
-use ears::{AudioController, Sound};
+use rodio::{Decoder, OutputStream, Sink};
+use std::fs::File;
+use std::io::BufReader;
 
 pub fn play_test() {
-    let mut sound = Sound::new("../samples/sample.wav").unwrap();
-    loop {
-        sound.play();
-        while sound.is_playing() {}
-    }
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    let file = BufReader::new(File::open("./samples/sample.wav").unwrap());
+    let source = Decoder::new(file).unwrap();
+
+    sink.append(source);
+    sink.sleep_until_end();
 }
